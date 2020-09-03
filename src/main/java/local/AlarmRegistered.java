b06a95f87eb073563121595f92fb325d3c11c7f7
heeyoungyun <67447253+heeyoungyun@users.noreply.github.com>
2020-09-03 15:16:02 +0900

    Create AlarmRegistered.java
    a

---
+++ src/main/java/local/AlarmRegistered.java
@@ -0,0 +1,32 @@
+package local;
+
+public class AlarmRegistered extends AbstractEvent {
+
+    private Long id;
+    private String receiver;
+    private String message;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
